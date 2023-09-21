64035d5a3e28e86c183479da10e854799c5a73ed
Larissa Evelyn <larissaevelyn554@gmail.com>
2023-09-21 11:27:29 -0300

    aula 21/09

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 buttton=document.getElementById("botao")
 .addEventListener('click', function(){
-    fetch("https://pokeapi.co/api/v2/pokemon/squirtle")
+    let nome = document.getElementById('nome-pokemon').value
+
+    fetch("https://pokeapi.co/api/v2/pokemon/" + nome)
     .then(function(response){
         return response.json()
     }).then(function(response){
+        console.log(response.abilities)
         console.log(response.sprites.front_default)
 
         let url = response.sprites.front_default
